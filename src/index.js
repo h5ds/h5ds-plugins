ecb9672e4192ad161cd406efc9c4c9641333ae38
mantou <676015863@qq.com>
2020-08-03 18:48:56 +0800

    add

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import zhCN from 'antd/es/locale/zh_CN';
 import { ConfigProvider } from 'antd';
-// import Editor from './Editor';
-import Editor from './Demo';
+import Editor from './Editor';
 import './utils/pubsub';
 import './style.less';
 
